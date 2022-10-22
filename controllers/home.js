904662d02647cb9873a403f889fcc7bcc04faebf
Boobeh123 <boobeh321@gmail.com>
2022-10-21 16:06:14 -1000

    Edit method to redirect authenticated users to /todos/

--- controllers/home.js
+++ controllers/home.js
@@ -1,5 +1,12 @@
 module.exports = {
     getIndex: (req,res)=>{
-        res.render('index.ejs')
+        if (req.isAuthenticated()) {
+            res.redirect('/todos')
+          } else {
+              res.render('index.ejs')
+          }
     }
+    // getIndex: (req,res)=>{
+    //     res.render('index.ejs')
+    // }
 }
