62ee375256aa0e99588c3dfbb97ce102a3776589
Boobeh123 <boobeh321@gmail.com>
2022-11-08 16:03:26 -1000

    Add GET & PUT request for /todos route

--- routes/todos.js
+++ routes/todos.js
@@ -10,9 +10,9 @@ router.get('/dueDate', ensureAuth, todosController.getDueDate)
 router.post('/createTodo', todosController.createTodo)
 
 router.put('/markComplete', todosController.markComplete)
-
+router.get('/edit/:id', todosController.editTodos)
+router.put('/:id', todosController.updateTodos)
 router.put('/markIncomplete', todosController.markIncomplete)
-// router.put('/editTodos', todosController.editTodos)
 
 router.delete('/deleteTodo', todosController.deleteTodo)
 
