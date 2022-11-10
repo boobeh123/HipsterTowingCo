384d13a725c7be5b9e97f59a86994b609e067513
Boobeh123 <boobeh321@gmail.com>
2022-11-09 16:57:06 -1000

    Require moment & method-override modules

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const MongoStore = require('connect-mongo')(session)
 const flash = require('express-flash')
 const logger = require('morgan')
 const connectDB = require('./config/database')
+const methodOverride = require("method-override");
 const mainRoutes = require('./routes/main')
 const todoRoutes = require('./routes/todos')
 
@@ -16,12 +17,15 @@ require('dotenv').config({path: './config/.env'})
 require('./config/passport')(passport)
 
 connectDB()
-
+// Allow moment to be used in templating engine
+app.locals.moment = require('moment')
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(logger('dev'))
+//Use forms for put / delete
+app.use(methodOverride("_method"));
 // Sessions
 app.use(
     session({
