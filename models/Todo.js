e74f787283d03c5fd4a410fc1c53720cafc15a55
Boobeh123 <boobeh321@gmail.com>
2022-11-12 17:05:01 -1000

    Add several new properties to TodoSchema

--- models/Todo.js
+++ models/Todo.js
@@ -1,7 +1,43 @@
 const mongoose = require('mongoose')
 
 const TodoSchema = new mongoose.Schema({
-  todo: {
+  contactNumber: {
+    type: Number,
+    required: true,
+  },
+  vehicleAddressPick: {
+    type: String,
+    required: true,
+  },
+  vehicleAddressDrop: {
+    type: String,
+  },
+  contactRideAlong: {
+    type: String,
+    required: true,
+  },
+  vehicleType: {
+    type: String,
+    required: true,
+  },
+  vehicleDoor: {
+    type: Number,
+    required: true,
+  },
+  vehicleColor: {
+    type: String,
+    required: true,
+  },
+  vehicleYear: {
+    type: Number,
+  },
+  vehicleMake: {
+    type: String,
+  },
+  vehicleModel: {
+    type: String,
+  },
+  vehicleLocation: {
     type: String,
   },
   completed: {
@@ -12,15 +48,10 @@ const TodoSchema = new mongoose.Schema({
     type: String,
     required: true
   },
-  todoInfo: {
-    type: String,
-  },
-  todoDate: {
+  createdAt: {
     type: Date,
+    default: Date.now()
   },
-  todoLevel: {
-    type: Number,
-  }
 })
 
 module.exports = mongoose.model('Todo', TodoSchema)
