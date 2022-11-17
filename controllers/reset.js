07b146c44b50fd2fdbcb0652b491b919989a611d
Boobeh123 <boobeh321@gmail.com>
2022-11-16 20:54:19 -1000

    Edit three methods - postPasswordRequest, getRecoverPassword & postRecoverPassword

--- controllers/reset.js
+++ controllers/reset.js
@@ -22,7 +22,7 @@ module.exports = {
             if (!validator.isEmail(req.body.email)) validationErrors.push({ msg: 'Please enter a valid email address.' })
             if (validationErrors.length) {
                 req.flash('errors', validationErrors)
-                return res.redirect('/recover')
+                return res.redirect('/forgot')
             }
             async.waterfall([
                 function (done) {
@@ -50,17 +50,16 @@ module.exports = {
                 },
                 function (token, user, done) {
                     const transporter = nodemailer.createTransport({
-                        service: "gmail",
-                        port: 587,
-                        secure: false,
+                        service: 'gmail',
                         auth: {
-                            user: process.env.USER,
-                            pass: process.env.PASS
-                        }
+                            user: process.env.EMAILNAME,
+                            pass: process.env.EMAILPASSWORD
+                        },
+                        tls : { rejectUnauthorized: false }
                     })
                     const mailOptions = {
                         to: user.email,
-                        from: process.env.USER,
+                        from: process.env.EMAILNAME,
                         subject: 'Bo-TOW Password Reset Request',
                         text: 'You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n' +
                               'Please click on the following link, or paste this into your browser to complete the process:\n\n' +
@@ -74,15 +73,15 @@ module.exports = {
                 }
             ], function (err) {
                 if (err) console.error(err);
-                res.redirect('/login');
+                res.redirect('/');
             })
         }
     },
-    getRecoverPassword: (req,res)=>{
+    getRecoverPassword: async (req,res)=>{
         if (req.isAuthenticated()) {
             res.redirect('/todos')
           } else {
-            res.render('recover.ejs')
+            res.render('recover.ejs', {token: req.params.token})
           }
     },
     postRecoverPassword: async (req,res)=>{
@@ -93,30 +92,30 @@ module.exports = {
             if (!validator.isLength(req.body.password, { min: 8 })) validationErrors.push({ msg: 'Password must be at least 8 characters long' })
             if (req.body.password !== req.body.confirmPassword) validationErrors.push({ msg: 'Passwords do not match' })
             if (validationErrors.length) {
-                req.flash('info', validationErrors)
+                req.flash('errors', validationErrors)
             }
             async.waterfall([
                 function (done) {
                     User.findOne({ resetPasswordToken: req.params.token, resetPasswordExpires: { $gt: Date.now() } }, function (err, user) {
                         if (!user) {
-                            req.flash('info', 'Password reset token is invalid or has expired.');
+                            req.flash('info', { msg: 'Password reset token is invalid or has expired.' });
                             return res.redirect('back');
                         }
-                        user.password = req.body.password;
-                        user.resetPasswordToken = undefined;
-                        user.resetPasswordExpires = undefined;
-                        user.save(function (err) {
-                            req.logIn(user, function (err) {
-                                done(err, user);
+                        if (req.body.password === req.body.confirmPassword) {
+                            user.password = req.body.password;
+                            user.resetPasswordToken = undefined;
+                            user.resetPasswordExpires = undefined;
+                            user.save(function (err) {
+                                req.logIn(user, function (err) {
+                                    done(err, user);
+                                });
                             });
-                        });
+                        }
                     });
                 },
             ], function (err) {
-                res.redirect('/');
+                res.redirect('/todos');
             })
         }
     }
-
-
 }
