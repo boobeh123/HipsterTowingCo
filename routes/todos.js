3c0d61e91b3b49b034b556ef813a4416080874e8
Boobeh123 <boobeh321@gmail.com>
2022-11-19 16:46:38 -1000

    Add 2 PUT requests to /assignTo/:id & /unassign/:id - Remove unused routes,

--- routes/todos.js
+++ routes/todos.js
@@ -4,15 +4,15 @@ const todosController = require('../controllers/todos')
 const { ensureAuth } = require('../middleware/auth')
 
 router.get('/', ensureAuth, todosController.getTodos)
-router.get('/sorted', ensureAuth, todosController.getSortedTodos)
-router.get('/dueDate', ensureAuth, todosController.getDueDate)
 
 router.post('/createTodo', todosController.createTodo)
 
 router.put('/markComplete/:id', todosController.markComplete)
+router.put('/markIncomplete/:id', todosController.markIncomplete)
 router.get('/edit/:id', todosController.editTodos)
 router.put('/:id', todosController.updateTodos)
-router.put('/markIncomplete', todosController.markIncomplete)
+router.put('/assignTo/:id', todosController.assignDriver)
+router.put('/unassign/:id', todosController.unassignDriver)
 
 router.delete('/deleteTodo/:id', todosController.deleteTodo)
 
