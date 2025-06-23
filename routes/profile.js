a6045ec9606ccffb102a19acd765d90c52670528
Bobby <boobeh321@gmail.com>
2025-06-22 17:37:42 -1000

    Create profile route & Add 4 routes - 2xGET, PUT, POST

---
+++ routes/profile.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const router = express.Router()
+const { ensureAuth, ensureGuest } = require('../middleware/auth')
+const profileController = require('../controllers/profile') 
+const upload = require("../middleware/multer");
+
+router.get("/", ensureAuth, profileController.getProfile);
+router.get("/edit/:id", ensureAuth, profileController.editProfile);
+router.put("/edit/:id", ensureAuth, profileController.updateProfile);
+router.post("/", ensureAuth, upload.single("file"), profileController.updatePhoto);
+
+module.exports = router
