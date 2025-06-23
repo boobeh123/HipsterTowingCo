b3b1e71d881b0ffbef88c852ae27cd7285740f74
Bobby <boobeh321@gmail.com>
2025-06-22 14:35:03 -1000

    Add /createInspection POST route. Add express-valdiator. Validate & Sanitize /createInspection

--- routes/todos.js
+++ routes/todos.js
@@ -1,12 +1,26 @@
 const express = require('express')
 const router = express.Router()
+const { body, validationResult } = require('express-validator');
 const todosController = require('../controllers/todos') 
 const { ensureAuth } = require('../middleware/auth')
 
 router.get('/', ensureAuth, todosController.getTodos)
 router.get('/filtered', todosController.getFilteredTodos)
 
-router.post('/createTodo', todosController.createTodo)
+// Validation and sanitization chain for /createInspection POST Route
+const createInspectionValidation = [
+    
+    body('truckTractorNo', 'USDOT # must be at least 3 characters long.')
+        .trim()
+        .isLength({ min: 3 }),
+
+    // Sanitize all other string fields to prevent XSS
+    body('trailerNo').trim().escape(),
+    body('remarks').trim().escape(),
+    body('defects.truckTractor.other').trim().escape(),
+    body('defects.trailer.other').trim().escape(),
+];
+router.post('/createInspection', ensureAuth, createInspectionValidation, todosController.createInspection)
 
 router.put('/markComplete/:id', todosController.markComplete)
 router.put('/markIncomplete/:id', todosController.markIncomplete)
