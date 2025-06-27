6bea9bfdb4d670c5792c3003e68a4ae2379b4ea2
Bobby <boobeh321@gmail.com>
2025-06-26 17:42:11 -1000

    Add /delete/:id route

--- routes/profile.js
+++ routes/profile.js
@@ -8,5 +8,6 @@ router.get("/", ensureAuth, profileController.getProfile);
 router.get("/edit/:id", ensureAuth, profileController.editProfile);
 router.put("/edit/:id", ensureAuth, profileController.updateProfile);
 router.post("/", ensureAuth, upload.single("file"), profileController.updatePhoto);
+router.delete("/delete/:id", ensureAuth, profileController.deleteProfile);
 
 module.exports = router
