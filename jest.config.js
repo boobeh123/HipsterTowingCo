de8c093e1d57dc3645384bac266231ab2d129cfa
Bobby <boobeh321@gmail.com>
2025-06-28 14:05:15 -1000

    Add/update Jest config

---
+++ jest.config.js
@@ -0,0 +1,35 @@
+module.exports = {
+  // Load test environment variables
+  setupFilesAfterEnv: ['<rootDir>/test/setup/setup.js'],
+  
+  // Test environment
+  testEnvironment: 'node',
+  
+  // Coverage settings
+  collectCoverage: false,
+  
+  // Test timeout
+  testTimeout: 15000,
+  
+  // Verbose output
+  verbose: true,
+  
+  // Clear mocks between tests
+  clearMocks: true,
+  
+  // Force exit after tests
+  forceExit: true,
+  
+  // Detect open handles
+  detectOpenHandles: true,
+  
+  // Test file patterns
+  testMatch: [
+    '**/test/**/*.test.js'
+  ],
+  
+  // Ignore patterns
+  testPathIgnorePatterns: [
+    '/node_modules/'
+  ]
+}; 
