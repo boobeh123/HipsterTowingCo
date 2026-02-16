658fb4038a7d732385b90f9536adc1c4f74d91d5
Bobby <boobeh321@gmail.com>
2026-02-15 21:59:38 -1000

    Update getIndex method

--- controllers/home.js
+++ controllers/home.js
@@ -1,9 +1,12 @@
 module.exports = {
-    getIndex: (req,res)=>{
-        if (req.isAuthenticated()) {
-            res.redirect('/todos')
-          } else {
-            res.render('index.ejs')
-          }
-    }
+
+    getIndex: async (req, res) => {
+      try {
+          res.render('index.ejs');
+    } catch(err) {
+        console.error(err)
+        res.status(500).render('500.ejs');
+      }
+    },
+
 }
