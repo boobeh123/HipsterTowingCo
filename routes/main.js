2124795d7e409f309459649cf7199e6a68f01cb9
Bobby <boobeh321@gmail.com>
2026-02-15 21:57:41 -1000

    Comment out most routes

--- routes/main.js
+++ routes/main.js
@@ -8,10 +8,10 @@ const termController = require('../controllers/terms')
 
 
 router.get('/', homeController.getIndex)
-router.get('/login', authController.getLogin)
+// router.get('/login', authController.getLogin)
 // router.post('/login', authController.postLogin)
 // router.get('/logout', authController.logout)
-router.get('/signup', authController.getSignup)
+// router.get('/signup', authController.getSignup)
 // router.post('/signup', authController.postSignup)
 // router.get('/forgot', passwordResetController.getPasswordReset)
 // router.post('/forgot', passwordResetController.postPasswordReset)
