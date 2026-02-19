f65e2643147ffb4fb0380d958480d816390b84dd
Bobby <boobeh321@gmail.com>
2026-02-18 20:12:20 -1000

    Correct types, add words/context

--- public/js/main.js
+++ public/js/main.js
@@ -46,7 +46,7 @@ function closeModal() {
 * 2. We iterate through the form submitted by the user. Logging the key reurns:
     * date truckTractorNo defects[truckTractor][steering]^ trailerNo defects[trailer][brakes]^ remarks mechanicDate driverDate
         * ^defects is an object and we are using bracket notation to access the object 
-* 3. We create a variable that uses the array method includes() to search the current key for a open bracket '['
+* 3. We create a variable that uses the array method includes() to search the current key for an open bracket '['
 * 4. We create a variable that uses the array methods split(), map(), replace() on the current key
     * 3 & 4 are solely used when the defects <input> contains value(s)
 * 5. We determine if the current key contains an open bracket
@@ -64,12 +64,12 @@ function closeModal() {
             * ^HOWEVER, replacing console.log(element) with element.replace(']', ''), returns an array of strings
                 *  Output: ['defects', 'truckTractorNo', 'Steering'] Take notice that ']' is replaced with an empty string
 
-7. We create two variables, one contains an expression that compares the current index to the number of elements within the array of strings
+7. We create two variables, one contains an expression that compares the current index to the number of elements within the array of strings. 2nd variable see: #10
 8. We determine if we are at the last element within the array
-* a. If we are at the last element: We determine if the value (1) contains a boolean or an empty string
-* We add the value to the user's inspection
-* b. Otherwise, we are not at the last element: We need to create an empty object and: 
-* c. We go deeper into the object until we reach the last element
+    * a. If we are at the last element: We determine if the value (1) compared to a boolean or an empty string evaluates as true
+        * Depending on the evaluation, we add the value(value,null,boolean) to the user's inspection
+    * b. Otherwise, we are not at the last element: We need to create an empty object and: 
+        * c. We go deeper into the object until we reach the last element, then see #8a
 9. We return an object and, (containing the value of every <input> from the form) pass it into validateAndSanitize() as an argument
 
 10. Additional explanation: We create a variable which points at the current element, at the start of the object
