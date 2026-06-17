3b36fcc7f3a02116739954bf20e3839eed1c1459
Bobby <boobeh321@gmail.com>
2026-06-17 12:10:03 -1000

    Update getIndex, getPrivacy, getTerms controller methods - remove fallback error handler

--- controllers/terms.js
+++ controllers/terms.js
@@ -4,8 +4,7 @@ module.exports = {
       try {
           res.render('terms.ejs');
     } catch(err) {
-        console.error(err)
-        res.status(500).render('500.ejs');
+      next(err)
       }
     },
 
