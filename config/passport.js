6ba3c28d89732fc4bb0d7ee0379ff6a71f6cbfb4
Bobby <boobeh321@gmail.com>
2026-06-16 18:25:14 -1000

    Refactor localStrategy

--- config/passport.js
+++ config/passport.js
@@ -1,32 +1,34 @@
 const LocalStrategy = require('passport-local').Strategy
-const mongoose = require('mongoose')
 const User = require('../models/User')
 
 module.exports = function (passport) {
-  passport.use(new LocalStrategy({ usernameField: 'email' }, (email, password, done) => {
-    User.findOne({ email: email.toLowerCase() })
-      .select('+password')
-      .exec((err, user) => {
+  passport.use(new LocalStrategy({ usernameField: 'email' }, function verify(email, password, done) {
+    User.findOne({ email: email }, function (err, user) {
+      if (err) { return done(err) }
+      if (!user) { return done(null, false, { message: 'Incorrect email or password.' }) }
+      user.comparePassword(password, function(err, isMatch) {
         if (err) { return done(err) }
-        if (!user) {
-          return done(null, false, { msg: `Email ${email} not found.` })
-        }
-        user.comparePassword(password, (err, isMatch) => {
-          if (err) { return done(err) }
-          if (isMatch) {
-            return done(null, user)
-          }
-          return done(null, false, { msg: 'Invalid email or password.' })
-        })
+        if (!isMatch) { return done(null, false, { message: 'Incorrect email or password.' }) }
+        return done(null, user)
       })
+    })
   }))
-  
 
-  passport.serializeUser((user, done) => {
-    done(null, user.id)
+  passport.serializeUser(function(user, done) {
+    process.nextTick(function() {
+      done(null, user.id)
+    })
   })
 
-  passport.deserializeUser((id, done) => {
-    User.findById(id, (err, user) => done(err, user))
+  passport.deserializeUser(function(id, done) {
+    process.nextTick(async function() {
+      try {
+        const user = await User.findById(id)
+        done(null, user)
+      } catch (err) {
+        done(err)
+      }
+    })
   })
+
 }
