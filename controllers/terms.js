94e859996d91b4e92b512d5e28612798f2a8d104
Bobby <boobeh321@gmail.com>
2026-06-17 10:32:08 -1000

    Update getTerms() method

--- controllers/terms.js
+++ controllers/terms.js
@@ -1,5 +1,12 @@
 module.exports = {
-    getTerms: (req, res) => {
-        res.render('terms');
-    }
+
+    getTerms: async (req, res) => {
+      try {
+          res.render('terms.ejs');
+    } catch(err) {
+        console.error(err)
+        res.status(500).render('500.ejs');
+      }
+    },
+
 }
