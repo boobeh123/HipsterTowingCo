cb0b6e4d77a53720d259f49471b64df4f60025b0
Bobby <boobeh321@gmail.com>
2026-06-17 10:31:54 -1000

    Update getPrivacy() method

--- controllers/privacy.js
+++ controllers/privacy.js
@@ -1,5 +1,12 @@
 module.exports = {
-    getPrivacy: (req, res) => {
-        res.render('privacy');
-    }
+
+    getPrivacy: async (req, res) => {
+      try {
+          res.render('privacy.ejs');
+    } catch(err) {
+        console.error(err)
+        res.status(500).render('500.ejs');
+      }
+    },
+
 }
