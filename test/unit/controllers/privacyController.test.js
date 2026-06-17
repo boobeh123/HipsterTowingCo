d379881d0caec7f79e8c57678a203d7706ab059b
Bobby <boobeh321@gmail.com>
2026-06-17 10:33:20 -1000

    Add/update test suites for /controllers/privacy.js

--- test/unit/controllers/privacyController.test.js
+++ test/unit/controllers/privacyController.test.js
@@ -1,10 +1,34 @@
 const privacyController = require('../../../controllers/privacy');
 
 describe('privacyController.getPrivacy', () => {
-  it('should render privacy', () => {
-    const req = {};
-    const res = { render: jest.fn() };
-    privacyController.getPrivacy(req, res);
-    expect(res.render).toHaveBeenCalledWith('privacy');
+  let req, res;
+
+  beforeEach(() => {
+    req = {};
+    res = {
+      render: jest.fn(),
+      status: jest.fn().mockReturnThis(),
+    };
   });
-}); 
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('should render privacy', async () => {
+    await privacyController.getPrivacy(req, res);
+
+    expect(res.render).toHaveBeenCalledWith('privacy.ejs');
+  });
+
+  it('should render 500 if res.render throws', async () => {
+    res.render
+      .mockImplementationOnce(() => { throw new Error('render failed') })
+      .mockImplementationOnce(() => {});
+
+    await privacyController.getPrivacy(req, res);
+
+    expect(res.status).toHaveBeenCalledWith(500);
+    expect(res.render).toHaveBeenCalledWith('500.ejs');
+  });
+});
