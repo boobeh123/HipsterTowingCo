d4f015421d176b2202e40eb593b029a1b901662f
Bobby <boobeh321@gmail.com>
2026-06-17 10:33:39 -1000

    Add/update test suites for /controllers/terms.js

--- test/unit/controllers/termsController.test.js
+++ test/unit/controllers/termsController.test.js
@@ -1,10 +1,34 @@
-const termsController = require('../../../controllers/terms');
+const termController = require('../../../controllers/terms');
 
-describe('termsController.getTerms', () => {
-  it('should render terms', () => {
-    const req = {};
-    const res = { render: jest.fn() };
-    termsController.getTerms(req, res);
-    expect(res.render).toHaveBeenCalledWith('terms');
+describe('termController.getTerms', () => {
+  let req, res;
+
+  beforeEach(() => {
+    req = {};
+    res = {
+      render: jest.fn(),
+      status: jest.fn().mockReturnThis(),
+    };
   });
-}); 
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('should render terms.ejs', async () => {
+    await termController.getTerms(req, res);
+
+    expect(res.render).toHaveBeenCalledWith('terms.ejs');
+  });
+
+  it('should render 500.ejs if res.render throws', async () => {
+    res.render
+      .mockImplementationOnce(() => { throw new Error('render failed') })
+      .mockImplementationOnce(() => {});
+
+    await termController.getTerms(req, res);
+
+    expect(res.status).toHaveBeenCalledWith(500);
+    expect(res.render).toHaveBeenCalledWith('500.ejs');
+  });
+});
