d99237c7bf34586bf5f8da7919dbfd791159ba02
Bobby <boobeh321@gmail.com>
2026-06-17 00:31:47 -1000

    Convert to async function

--- config/passport.js
+++ config/passport.js
@@ -2,16 +2,18 @@ const LocalStrategy = require('passport-local').Strategy
 const User = require('../models/User')
 
 module.exports = function (passport) {
-  passport.use(new LocalStrategy({ usernameField: 'email' }, function verify(email, password, done) {
-    User.findOne({ email: email }, function (err, user) {
-      if (err) { return done(err) }
+  passport.use(new LocalStrategy({ usernameField: 'email' }, async function verify(email, password, done) {
+    try {
+      const user = await User.findOne({ email: email })
       if (!user) { return done(null, false, { message: 'Incorrect email or password.' }) }
       user.comparePassword(password, function(err, isMatch) {
         if (err) { return done(err) }
         if (!isMatch) { return done(null, false, { message: 'Incorrect email or password.' }) }
         return done(null, user)
       })
-    })
+    } catch (err) {
+      return done(err)
+    }
   }))
 
   passport.serializeUser(function(user, done) {
