e57f5ae477a26506593dc419fdae43f10de08baf
Bobby <boobeh321@gmail.com>
2026-06-16 18:25:51 -1000

    Add postLogin & postSignup methods

--- controllers/auth.js
+++ controllers/auth.js
@@ -5,31 +5,99 @@ const User = require('../models/User')
 module.exports = {
 
   getLogin: async (req, res) => {
-
     try {
       if (req.user) {
         return res.redirect('/')
       } else {
-        res.render('login.ejs');
+        res.render('login.ejs')
       }
-  } catch(err) {
+    } catch(err) {
       console.error(err)
-      res.status(500).render('500.ejs');
+      res.status(500).render('500.ejs')
     }
+  },
+
+  postLogin: (req, res, next) => {
+    const validationErrors = []
+    if (!validator.isEmail(req.body.email)) validationErrors.push({ msg: 'Please enter a valid email address.' })
+    if (!validator.isLength(req.body.password, { min: 5 })) validationErrors.push({ msg: 'Password must be at least 5 characters long.' })
+
+    if (validationErrors.length) {
+      req.flash('errors', validationErrors)
+      return res.redirect('/login')
+    }
+    req.body.email = validator.normalizeEmail(req.body.email, { gmail_remove_dots: false })
+
+    passport.authenticate('local', (err, user, info) => {
+      if (err) { return next(err) }
+      if (!user) {
+        req.flash('errors', [{ msg: info.message || 'Incorrect email or password.' }])
+        return res.redirect('/login')
+      }
+      req.logIn(user, (err) => {
+        if (err) { return next(err) }
+        return res.redirect('/')
+      })
+    })(req, res, next)
+  },
+
+  logout: (req, res, next) => {
+    req.logout(function(err) {
+      if (err) { return next(err) }
+      res.redirect('/')
+    })
   },
 
   getSignup: async (req, res) => {
-
     try {
       if (req.user) {
         return res.redirect('/')
       } else {
-        res.render('signup.ejs');
+        res.render('signup.ejs')
       }
-  } catch(err) {
+    } catch(err) {
       console.error(err)
-      res.status(500).render('500.ejs');
+      res.status(500).render('500.ejs')
     }
-
   },
+
+  postSignup: async (req, res, next) => {
+    const validationErrors = []
+    if (!validator.isEmail(req.body.email)) validationErrors.push({ msg: 'Please enter a valid email address.' })
+    if (!validator.isLength(req.body.password, { min: 5 })) validationErrors.push({ msg: 'Password must be at least 5 characters long.' })
+    if (req.body.password !== req.body.confirmPassword) validationErrors.push({ msg: 'Passwords do not match.' })
+
+    if (validationErrors.length) {
+      req.flash('errors', validationErrors)
+      return res.redirect('/signup')
+    }
+    req.body.email = validator.normalizeEmail(req.body.email, { gmail_remove_dots: false })
+
+    try {
+      const existingUser = await User.findOne({ email: req.body.email })
+      if (existingUser) {
+        req.flash('errors', [{ msg: 'An account with that email address already exists.' }])
+        return res.redirect('/signup')
+      }
+
+      const user = new User({
+        role: 'user',
+        email: req.body.email,
+        password: req.body.password,
+        name: '',
+        image: '',
+        cloudinaryId: '',
+      })
+
+      await user.save()
+
+      req.login(user, function(err) {
+        if (err) { return next(err) }
+        res.redirect('/')
+      })
+    } catch(err) {
+      return next(err)
+    }
+  },
+
 }
