93894440a5f4616a0d11d319e3d54df82771a8ff
Bobby <boobeh321@gmail.com>
2026-06-18 13:29:18 -1000

    Update path

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -7,5 +7,5 @@
  **************************************************************/
 module.exports = (err, req, res, next) => {
     console.error(err.stack)
-    res.status(500).render('500.ejs')
+    res.status(500).render('errors/500.ejs')
 }
