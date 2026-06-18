f6d74d76f9c35802721d2a1115a425bfdc968f8f
Bobby <boobeh321@gmail.com>
2026-06-17 14:15:54 -1000

    Update getIndex to render dynamic data, Add postInspectionCount method

--- controllers/home.js
+++ controllers/home.js
@@ -1,11 +1,32 @@
+const Counter = require('../models/Counter')
+
 module.exports = {
 
-    getIndex: async (req, res) => {
-      try {
-          res.render('index.ejs');
-    } catch(err) {
-      next(err)
-      }
+    getIndex: async (req, res, next) => {
+        try {
+            const counter = await Counter.findOne({ name: 'inspectionCount' })
+            const inspectionCount = counter ? counter.value : 0
+            res.render('index.ejs', { inspectionCount })
+        } catch(err) {
+            next(err)
+        }
+    },
+
+    /**************************************************************
+    * postInspectionCount()
+    * Increments the inspectionCount counter by +1 after a PDF is successfully generated
+    **************************************************************/
+    postInspectionCount: async (req, res, next) => {
+        try {
+            const counter = await Counter.findOneAndUpdate(
+                { name: 'inspectionCount' },
+                { $inc: { value: 1 } },
+                { new: true, upsert: true }
+            )
+            res.json({ count: counter.value })
+        } catch(err) {
+            next(err)
+        }
     },
 
 }
