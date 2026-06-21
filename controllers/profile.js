974eb2eb8a2bdee4588b54fda6a2b23ddfa0a2a5
Bobby <boobeh321@gmail.com>
2026-06-20 16:42:58 -1000

    Add getProfile, updatePhoto, deleteAccount controller methods

--- controllers/profile.js
+++ controllers/profile.js
@@ -1,124 +1,69 @@
 const User = require('../models/User')
-const cloudinary = require("../middleware/cloudinary");
+const cloudinary = require('../middleware/cloudinary')
 
 module.exports = {
-    getProfile: async (req, res) => {
+
+    getProfile: async (req, res, next) => {
         try {
-            let pageName = 'Profile'
-            let allUsers = await User.find().lean();
-            res.render("profile.ejs", {
-                user: req.user,
-                users: allUsers,
-                page: pageName
-            });
-        } catch (err) {
-        console.log(err);
-        res.render('./errors/500.ejs')
-        }
-    },
-    editProfile: async (req, res) => {
-        try {
-            let pageName = 'Profile'
-            let loggedInUser = await User.findById(req.user._id).lean()
-            if (!loggedInUser) {
-                res.redirect('/')
-            }
-            if (loggedInUser.email !== req.user.email) {
-                res.redirect('/')
-            } else {
-                res.render('editProfile', {
-                user:req.user,
-                page: pageName})
-            }
+            res.render('profile.ejs')
         } catch(err) {
-            console.log(err);
-        res.render('./errors/500.ejs')
+            next(err)
         }
     },
-    updateProfile: async (req, res) => {
+
+    updatePhoto: async (req, res, next) => {
         try {
-            let loggedInUser = await User.findById(req.user._id).lean()
-            if (!loggedInUser) {
-                res.redirect('/')
-            }
-            if (loggedInUser.email !== req.user.email) {
-                res.redirect('/')
-            } else {
-                await User.findByIdAndUpdate(req.user._id, {
-                    name: req.body.userName,
-                    email: req.body.userEmail,
-            })
-                console.log('Edited Profile Information')
-                res.redirect('/profile')
-            }
-        } catch(err) {
-        console.error(err)
-        res.render('./errors/404.ejs')
-        }
-    },
-    updatePhoto: async (req, res) => {
-        try {
-            let imageUrl;
-            if (req.user.image === '') {
-                const result = await cloudinary.uploader.upload(req.file.path, {
-                    width: 100, 
-                    height: 100, 
-                    gravity: "faces", 
-                    crop: "thumb"
-                });
-                await User.findByIdAndUpdate(req.user.id, {
-                    image: result.secure_url,
-                    cloudinaryId: result.public_id
-                })
-                imageUrl = result.secure_url;
-            } else {
+            if (req.user.cloudinaryId) {
                 await cloudinary.uploader.destroy(req.user.cloudinaryId)
-                const result = await cloudinary.uploader.upload(req.file.path, {
-                    width: 100, 
-                    height: 100, 
-                    gravity: "faces", 
-                    crop: "thumb"
-                });
-                await User.findByIdAndUpdate(req.user.id, {
-                    image: result.secure_url,
-                    cloudinaryId: result.public_id
-                })
-                imageUrl = result.secure_url;
             }
-            console.log('Profile picture added');
-            if (
-                req.xhr ||
-                (req.headers.accept && req.headers.accept.includes('application/json')) ||
-                (req.headers['content-type'] && req.headers['content-type'].includes('application/json'))
-              ) {
-                return res.json({ imageUrl });
-              }
-            res.redirect('/profile');
+
+            const result = await cloudinary.uploader.upload(req.file.path, {
+                width: 200,
+                height: 200,
+                gravity: 'faces',
+                crop: 'thumb',
+            })
+
+            await User.findByIdAndUpdate(req.user._id, {
+                image: result.secure_url,
+                cloudinaryId: result.public_id,
+            })
+
+            req.flash('success', 'Profile photo updated.')
+            req.session.save((err) => {
+                if (err) { return next(err) }
+                res.redirect('/profile')
+            })
         } catch(err) {
-            console.error(err);
-            if (req.xhr || req.headers.accept.indexOf('json') > -1) {
-                return res.status(500).json({ error: 'Error uploading photo.' });
-            }
-            res.render('./errors/500.ejs')
+            next(err)
         }
     },
-    deleteProfile: async (req, res) => {
+
+    deleteAccount: async (req, res, next) => {
         try {
-            // Only allow users to delete their own profile
-            if (req.user._id.toString() !== req.params.id) {
-                return res.status(403).render('./errors/404.ejs');
+            const Inspection = require('../models/Inspection')
+
+            // Remove Cloudinary asset if present
+            if (req.user.cloudinaryId) {
+                await cloudinary.uploader.destroy(req.user.cloudinaryId)
             }
-            await User.findByIdAndDelete(req.user._id);
-            req.logout(() => {
-                req.session.destroy((err) => {
-                    if (err) console.log('Error : Failed to destroy the session during profile deletion.', err);
-                    req.user = null;
-                    res.redirect('/');
-                });
-            });
-        } catch (err) {
-            console.error(err);
-            res.render('./errors/500.ejs');
+
+            // Delete all inspections belonging to this user
+            await Inspection.deleteMany({ userId: req.user._id })
+
+            // Delete the user document
+            await User.findByIdAndDelete(req.user._id)
+
+            req.logout(function(err) {
+                if (err) { return next(err) }
+                req.session.destroy(function(err) {
+                    if (err) { return next(err) }
+                    res.redirect('/')
+                })
+            })
+        } catch(err) {
+            next(err)
         }
-    }
+    },
+
 }
