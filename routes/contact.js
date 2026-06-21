a11e6f6df74eac52421c6e842bac5ea0d414dba8
Bobby <boobeh321@gmail.com>
2026-06-20 19:09:28 -1000

    Remove unused route/controller

--- routes/contact.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const contactController = require('../controllers/contact');
-
-// router.post('/submit', contactController.submit);
-
-module.exports = router; 
