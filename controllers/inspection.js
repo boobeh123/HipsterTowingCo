a39f03fb209e75f348c22cf5f7b5a6c96eda5b5a
Bobby <boobeh321@gmail.com>
2026-06-24 21:25:49 -1000

    Add deleteInspection method & Add/move getInspection method from dashboard controller

--- controllers/inspection.js
+++ controllers/inspection.js
@@ -2,6 +2,23 @@ const Inspection = require('../models/Inspection')
 
 module.exports = {
 
+    getInspection: async (req, res, next) => {
+        try {
+            const inspection = await Inspection.findOne({
+                _id: req.params.id,
+                userId: req.user._id,
+            }).lean()
+
+            if (!inspection) {
+                return res.status(404).json({ error: 'Inspection not found.' })
+            }
+
+            res.json(inspection)
+        } catch(err) {
+            next(err)
+        }
+    },
+
     postInspection: async (req, res, next) => {
         try {
             const inspection = new Inspection({
@@ -15,6 +32,31 @@ module.exports = {
                 const message = Object.values(err.errors)[0]?.message || 'Validation failed.'
                 return res.status(400).json({ error: message })
             }
+            next(err)
+        }
+    },
+
+    deleteInspection: async (req, res, next) => {
+        try {
+            const inspection = await Inspection.findOneAndDelete({
+                _id: req.params.id,
+                userId: req.user._id,
+            })
+
+            if (!inspection) {
+                req.flash('errors', [{ msg: 'Inspection not found.' }])
+                return req.session.save((err) => {
+                    if (err) return next(err)
+                    res.redirect('/dashboard')
+                })
+            }
+
+            req.flash('success', 'Inspection deleted.')
+            req.session.save((err) => {
+                if (err) return next(err)
+                res.redirect('/dashboard')
+            })
+        } catch(err) {
             next(err)
         }
     },
