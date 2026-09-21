f85f1c30f6e3378de458f3eae9b13888aeae3ad5
Bobby <boobeh321@gmail.com>
2026-09-20 16:42:09 -1000

    Update postLogin - Remove validation on password length & Update - postSignup - Update min password length

--- controllers/auth.js
+++ controllers/auth.js
@@ -18,7 +18,6 @@ module.exports = {
   postLogin: (req, res, next) => {
     const validationErrors = []
     if (!validator.isEmail(req.body.email)) validationErrors.push({ msg: 'Please enter a valid email address.' })
-    if (!validator.isLength(req.body.password, { min: 5 })) validationErrors.push({ msg: 'Password must be at least 5 characters long.' })
 
     if (validationErrors.length) {
       req.flash('errors', validationErrors)
@@ -73,7 +72,7 @@ module.exports = {
   postSignup: async (req, res, next) => {
     const validationErrors = []
     if (!validator.isEmail(req.body.email)) validationErrors.push({ msg: 'Please enter a valid email address.' })
-    if (!validator.isLength(req.body.password, { min: 5 })) validationErrors.push({ msg: 'Password must be at least 5 characters long.' })
+    if (!validator.isLength(req.body.password, { min: 8 })) validationErrors.push({ msg: 'Password must be at least 8 characters long.' })
     if (req.body.password !== req.body.confirmPassword) validationErrors.push({ msg: 'Passwords do not match.' })
 
     if (validationErrors.length) {
